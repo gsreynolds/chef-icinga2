0bea306335e9d34cf7472e4f59a8bc0677619b45
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2017-08-22 14:45:37 +0100

    Remove version constraints and fix apache2 mod_php5 -> mod_php

--- metadata.rb
+++ metadata.rb
@@ -10,13 +10,13 @@ version '2.9.4'
 source_url 'https://github.com/Icinga/chef-icinga2' if respond_to?(:source_url)
 issues_url 'https://dev.icinga.org/projects/chef-icinga2' if respond_to?(:issues_url)
 
-depends 'apt', '~> 5.0.1'
-depends 'yum', '~> 4.1.0'
-depends 'yum-epel', '~> 2.1.1'
-depends 'apache2', '~> 3.2.2'
-depends 'ulimit', '~> 0.4.0'
-depends 'pnp4nagios', '~> 0.2.8'
-depends 'chocolatey', '~> 1.0.3'
+depends 'apt'
+depends 'yum'
+depends 'yum-epel'
+depends 'apache2'
+depends 'ulimit'
+depends 'pnp4nagios'
+depends 'chocolatey'
 
 %w(redhat centos amazon ubuntu debian windows).each do |os|
   supports os
